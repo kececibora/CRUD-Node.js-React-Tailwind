5e9b4e7b0f7ada2f9525112339b858e718d77a50
Ramazan Bora Keçeci <boraizzet0@gmail.com>
2022-11-27 18:07:59 +0300

    Tailwind.css loaded..

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./Home.css";
+
 const Home = () => {
   return (
-    <div className="home">
-      <h1>Home</h1>
+    <div>
+      <h1 className="flex justify-center text-9xl">Home</h1>
       <ToastContainer position="top-center" />
     </div>
   );
